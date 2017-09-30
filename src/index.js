2f5b5cbeef9a3bedc1df0b5cdf3b8710a7cc0118
Benjamin Wasty <benny.wasty@gmail.com>
2017-09-30 11:43:15 +0200

    add slightly improved version of ianjsikes/rust-wasm-webpack-tutorial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const wasm = require('./main.rs')
+
+wasm.initialize({noExitRuntime: true}).then(module => {
+  // Create a Javascript wrapper around our Rust function
+  const multiply = module.cwrap('multiply', 'number', ['number', 'number'])
+
+  document.body.innerHTML = `Calling Rust multiply(7, 6)<br>${multiply(7, 6)}`
+})
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, browser! [from Rust main]");
+}
+
+// Functions that you wish to access from Javascript
+// must be marked as no_mangle
+#[no_mangle]
+pub fn multiply(a: i32, b: i32) -> i32 {
+    return a * b
 }
